09206268ac93d732fbb631a55c5c572e2bc47d7d
ejjang2030 <ejjang2030@gmail.com>
2026-06-15 13:16:57 +0900

    fix : 호출 주소 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const internalApiBaseUrl =
+  process.env.NEXT_INTERNAL_API_BASE_URL ?? "http://localhost:5001/api";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${internalApiBaseUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
